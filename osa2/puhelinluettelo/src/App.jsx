d123969380309e1492d7804d89c19f9ac5f8a29c
Emma Järvinen <emma.k.jarvinen@hotmail.com>
2024-11-28 17:48:16 +0200

    Puhelinluettelo luotu ja 2.6 tehty

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+const Person = (props) => {
+  return (
+    <p>{props.name}</p>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName,
+      id: newName
+    }
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addName}>
+
+        <div>
+          name: 
+          <input value={newName}
+          onChange={handleNameChange}/>
+
+        </div>
+        
+        <div>
+          <button type="submit">add</button>
+        </div>
+
+      </form>
+
+      <h2>Numbers</h2>
+      
+      <ul>
+        {persons.map(person => 
+          <Person key={person.name} name={person.name}/>
+        )}
+      </ul>
+
+    </div>
+  )
+
+}
+
+export default App
