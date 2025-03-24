70bf0010b39172e228cd3a107cf9524c43896a30
Emma Järvinen <emma.k.jarvinen@hotmail.com>
2025-03-24 11:44:28 +0200

    3.3, 3.4, 3.5 ja 3.6 tehty

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -27,13 +27,83 @@ let persons = [
     }
 ]
 
+app.use(express.json())
+
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+    response.send('<h1>Puhelinluettelo</h1>')
 })
   
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(note => note.id !== id)
+  
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const id = Math.floor(Math.random() * 1000)
+    return(String(id))
+}
+
+
+app.post('/api/persons', (request, response) => {
+
+    const body = request.body
+
+    const names = persons.map(n => n.name)
+
+    if (!body.name & !body.number) {
+        return response.status(400).json({ 
+          error: 'name and number missing' 
+        })
+    }
+    
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'name missing' 
+      })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({ 
+          error: 'number missing' 
+        })
+    }
+
+    if (names.includes(body.name)){
+        return response.status(400).json({ 
+          error: 'name must be unique' 
+        })
+    }
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+})
+
+
 
 app.get('/info', (request, response) => {
 
@@ -42,7 +112,7 @@ app.get('/info', (request, response) => {
     const pvm = new Date()
 
     response.send(
-       `Phonebook has info for ${maara} persons <br><br> 
+       `Phonebook has info for ${maara} people <br><br> 
        ${pvm}`
     )
 })
