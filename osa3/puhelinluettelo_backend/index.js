0b19a78c94264f2c4fa91d80e0f28fb68e55aada
Emma Järvinen <emma.k.jarvinen@hotmail.com>
2025-03-25 13:11:35 +0200

    3.9 tehty, backend valmiina Renderiin

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
+//const cors = require('cors')
+
 const app = express()
 
 let persons = [
@@ -132,6 +134,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
