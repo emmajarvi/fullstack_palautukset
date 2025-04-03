21680499ed832e4a80c39db4d2971119579cc206
Emma Järvinen <emma.k.jarvinen@hotmail.com>
2025-04-03 12:48:29 +0300

    Esimerkkien muutokset ajantasalle

--- osa3/esimerkit/index.js
+++ osa3/esimerkit/index.js
@@ -1,30 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
+const Note = require('./models/note')
 
-const password = process.argv[2]
-
+/*
 const url = `mongodb+srv://fullstack:${password}@cluster0.e7osvqp.mongodb.net/noteApp?
              retryWrites=true&w=majority&appName=Cluster0`
+*/
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
-          
 let notes = [
   {
     id: "1",
@@ -57,15 +40,9 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
-
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -75,12 +52,14 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
+/*
 const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => Number(n.id)))
     : 0
   return String(maxId + 1)
 }
+*/
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -91,18 +70,17 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
-    id: generateId(),
-  }
+    important: body.important || false
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
