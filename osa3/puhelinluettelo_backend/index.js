636f84518cb6551987c6b9407ca9e86ca2db43dc
Emma Järvinen <emma.k.jarvinen@hotmail.com>
2025-10-27 20:54:32 +0200

    uibuild

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -77,7 +77,7 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     }).catch(error => {
-      console.log(error.response.data)
+      console.log(error.message)
     })
     
 })
