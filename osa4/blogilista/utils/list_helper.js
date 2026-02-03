1c0d5875880749eca9081b946346e53aa7bd39a2
Emma Järvinen <emma.k.jarvinen@hotmail.com>
2026-02-03 15:15:02 +0200

    4.5 tehty

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -10,10 +10,22 @@ const totalLikes = (blogs) => {
 
   return blogs.length === 0 
   ? 0 
-  :blogs.reduce(reducer, 0) 
+  : blogs.reduce(reducer, 0) 
+}
+
+const favoriteBlog = (blogs) => {
+  const reducer = (prevItem, newItem) => {
+    if (prevItem.likes > newItem.likes) return prevItem
+    else return newItem
+  }
+
+  return blogs.length === 0 
+  ? 0 
+  : blogs.reduce(reducer, blogs[0])
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
