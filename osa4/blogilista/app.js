235c2ee0222462c6e6334d4c8f1300e6beac9be3
Emma Järvinen <emma.k.jarvinen@hotmail.com>
2026-03-30 19:46:40 +0300

    4.15 tehty

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware.js')
 const blogsRouter = require('./controllers/blogs.js')
 const usersRouter = require('./controllers/users.js')
+const loginRouter = require('./controllers/login.js')
 
 const app = express()
 
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
