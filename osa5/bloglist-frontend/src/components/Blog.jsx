deaddc98bdda531ddeb553947e924805fd7986c7
Emma Järvinen <emma.k.jarvinen@hotmail.com>
2026-05-22 12:41:51 +0300

    5.7 tehty

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import Togglable from "./Toggalble"
+
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 5,
+    paddingBottom: 10,
+    paddingLeft: 10,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <p style={{ fontWeight: 'bold' }}>{blog.title}</p>
+      <Togglable buttonLabel='näytä tiedot'>
+        <div>
+          {blog.url} 
+          <br/>
+          tykkäyksiä: {blog.likes} {' '}
+          <button>tykkää</button>
+          <br/>
+          {blog.author}
+        </div>
+        <br/>
+      </Togglable>
+    </div>
+  )
+}
 
 export default Blog
