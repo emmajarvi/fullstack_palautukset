390de143490466e6fc808c5460bad0f8a098918b
Emma Järvinen <emma.k.jarvinen@hotmail.com>
2026-05-25 21:39:55 +0300

    5.15 ja 5.16 tehty

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import blogService from '../services/blogs'
+
+vi.mock('../services/blogs')
 
 test('renders title', () => {
   const blog = {
@@ -42,3 +45,28 @@ test('clicking the show more info button renders rest of the blog information',
 
   }
 )
+
+test('clicking the like button twice calls the event handler twice', async () => {
+  const blog = {
+    id: '1',
+    title: 'A test blog for testing',
+    author: 'example example',
+    url: 'example.com',
+    likes: 0,
+    user: { id: '123', username: 'testuser' }
+  }
+
+  blogService.update.mockResolvedValue({ ...blog, likes: 1 })
+
+  render(<Blog blog={blog} setBlogs={() => {}} setMessage={() => {}} user={null} />)
+
+  const user = userEvent.setup()
+  const showButton = screen.getByText('näytä tiedot')
+  await user.click(showButton)
+
+  const likeButton = screen.getByText('tykkää')
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(blogService.update).toHaveBeenCalledTimes(2)
+})
